8e11a1fcc1721f6b44a9b0c16406e4f492846f88
Trung Do <chin.bimbo@gmail.com>
2016-07-28 20:42:07 -0400

    Rookie Rank

---
+++ src/hackerRank/contests/rookieRank/BirthdayCakeCandles.java
@@ -0,0 +1,28 @@
+package hackerRank.contests.rookieRank;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by tdo on 26-Jul-16.
+ */
+public class BirthdayCakeCandles {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int max = -1;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int h = in.nextInt();
+            if (h >= max) {
+                if (h > max) {
+                    count = 0;
+                }
+                max = h;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
